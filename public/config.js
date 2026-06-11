531b236ad4c5a065ce3977fa5ed95f17f9560170
rain8777 <120436557+rain8777@users.noreply.github.com>
2026-06-11 11:49:06 +0800

    Add files via upload

--- public/config.js
+++ public/config.js
@@ -1,1 +1,1 @@
-window.AQUATRACK_GAS_URL = "https://script.google.com/macros/s/AKfycbwSgKC9vHQpTjXxbSzopERtxUHRJK5jOacasZat5kHR0eT0yzLOIMopTLm5XjPgeehG/exec";
+window.AQUATRACK_GAS_URL = "YOUR_GAS_URL_HERE";
